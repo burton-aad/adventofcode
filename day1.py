9b538c1b6f9ae18a86e82bd32f7cb13e3ce84a5f
Alexandre Adolphe <Alexandre Adolphe>
2016-12-29 02:25:16 +0100

    Make solution for part2 of day 1

--- day1.py
+++ day1.py
@@ -4,6 +4,8 @@ from __future__ import print_function
 HORZ = 0
 VERT = 1
 pos = (0, 0)
+all_pos = []
+check_cross = True
 axe = 'N'
 rose = "NESW"
 move = {
@@ -14,13 +16,23 @@ move = {
 }
 
 def input_move(turn, length):
-    global axe, pos
+    global axe, pos, all_pos, check_cross
     i = rose.index(axe)
     if turn == 'R':
         i = (i+1) % len(rose)
     else: # turn == 'L'
         i = (i-1) % len(rose)
     axe = rose[i]
+    if check_cross:
+        old_pos = pos
+        for i in range(length):
+            new_pos = move[axe](old_pos, i)
+            if new_pos in all_pos:
+                print("First cross at", new_pos, ":", abs(new_pos[0]) + abs(new_pos[1]), "Blocks away")
+                check_cross = False
+                break
+            else:
+                all_pos.append(new_pos)
     pos = move[axe](pos, length)
 
 with open("input1") as f:
