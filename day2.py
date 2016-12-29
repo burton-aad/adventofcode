9b842d1f7f06616e028e1fa08c378becd0cdaca4
Alexandre Adolphe <Alexandre Adolphe>
2016-12-29 02:33:04 +0100

    Make second part of day2

--- day2.py
+++ day2.py
@@ -1,5 +1,9 @@
 
-pad = ((1,2,3), (4,5,6), (7,8,9))
+pad1 = ((1,2,3), (4,5,6), (7,8,9))
+pad2 = (('*', '*', '1', '*', '*'), ('*', '2', '3', '4', '*'), ('5', '6', '7', '8', '9'), ('*', 'A', 'B', 'C', '*'), ('*', '*', 'D', '*', '*'))
+pad = pad2
+max_row = len(pad)-1
+max_col = len(pad[0])-1
 row = 1
 col = 1
 
@@ -9,12 +13,12 @@ with open("input2") as f:
             continue
         for d in l:
             if d == 'U':
-                row = row-1 if row > 0 else row
+                row = row-1 if row > 0 and pad[row-1][col] != '*' else row
             elif d == 'R':
-                col = col+1 if col < 2 else col
+                col = col+1 if col < max_col and pad[row][col+1] != '*' else col
             elif d == 'L':
-                col = col-1 if col > 0 else col
+                col = col-1 if col > 0 and pad[row][col-1] != '*' else col
             elif d == 'D':
-                row = row+1 if row < 2 else row
+                row = row+1 if row < max_row and pad[row+1][col] != '*' else row
         print pad[row][col]
 
