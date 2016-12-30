721dd5af353b3e2659b1b6cb8c174a83a7c5b000
Alexandre Adolphe <Alexandre Adolphe>
2016-12-30 02:50:36 +0100

    And day6b in a row :)

--- day6.py
+++ day6.py
@@ -5,10 +5,14 @@ def max_letter(t):
     c = Counter(t)
     return c.most_common(1)[0][0]
 
+def min_letter(t):
+    c = Counter(t)
+    return c.most_common()[-1][0]
+
 
 with open('input6') as f:
     m = zip(*f.readlines())
     t = []
     for l in m:
-        t.append(max_letter(l))
+        t.append(min_letter(l))
     print "".join(t)
