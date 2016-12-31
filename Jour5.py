f2b75a3a7d844722ce166519cb3799d4a4f4c829
Alexandre Adolphe <Alexandre Adolphe>
2016-12-31 14:43:51 +0100

    Deux version pour Jour5 en une fois.

--- Jour5.py
+++ Jour5.py
@@ -3,14 +3,20 @@ import hashlib
 
 input5 = "ojvtpuvg"
 
-code = []
+codeA = []
+codeB = ['-'] * 8
 index = 0
 
-while len(code) < 8:
-    h = hashlib.md5(input5+str(index))
-    if h.hexdigest().startswith("00000"):
-        code.append(h.hexdigest()[5])
-        print index, h.hexdigest(), code[-1]
+while '-' in codeB:
+    h = hashlib.md5(input5+str(index)).hexdigest()
+    if h.startswith("00000"):
+        if len(codeA) < 8:
+            codeA.append(h[5])
+        i = int(h[5], 16)
+        if i < len(codeB) and codeB[i] == '-':
+            codeB[i] = h[6]
+        print index, h, h[5], codeB
     index += 1
 
-print "".join(code)
+print "A:", "".join(codeA)
+print "B:", "".join(codeB)
