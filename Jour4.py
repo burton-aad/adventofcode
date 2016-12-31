f8ccc23d16b515032c06d7202761f9aa56e0c25c
Alexandre Adolphe <Alexandre Adolphe>
2016-12-31 11:35:43 +0100

    Amélioration du jour 4

--- Jour4.py
+++ Jour4.py
@@ -1,27 +1,30 @@
 
 from collections import Counter
+import re
 
-def is_valid_room(s):
-    p = s.rfind('-')
-    q = s.find('[')
-    name = s[:p].translate(None, '-')
-    sid = s[p+1:q]
-    h = s[q+1:-1]
+r = re.compile("([a-z-]+)([0-9]+)\[([a-z]+)\]")
+
+def extract(s):
+    global r
+    m = r.match(s)
+    return m.groups()
+
+def is_valid_room(name, h):
     c = Counter(name).items()
     c.sort(key=lambda x: x[0])
     c.sort(key=lambda x: x[1], reverse=True)
     test_h = "".join(map(lambda x: x[0], c))[:5]
     #print test_h
-    if test_h == h:
-        return True, sid
-    else:
-        return False, sid
+    return test_h == h
 
 with open('input4') as f:
     c = 0
     for l in f:
-        t, sid = is_valid_room(l.strip())
-        if t:
+        l = l.strip()
+        if len(l) == 0:
+            continue
+        name, sid, h = extract(l)
+        if is_valid_room(name.translate(None, '-'), h):
             c += int(sid)
     print c
 
