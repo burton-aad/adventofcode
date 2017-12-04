ef56a27149abfa51b9c9115b7d4c74bb5a712b48
Alexandre Adolphe <Alexandre_Adolphe@sigmadesigns.com>
2017-12-04 19:11:06 +0100

    Jour3 partie 2

--- 2017/Jour3.py
+++ 2017/Jour3.py
@@ -8,27 +8,24 @@ up = 1
 left = 2
 down = 3
 
-def step_from_value(v):
-    i = 0
-    r = 1
-    while r < v:
-        i += 1
-        r += 8*i
-    return i
+def next_v_1(s, x, y, v):
+    return v + 1
 
-def create_spiral_tour(tour=0):
+def create_spiral_tour(tour, limit=0, next_v=next_v_1):
     line_w = 2*(tour)+1
     s = [[0] * line_w for _ in range(line_w)]
     x, y = line_w/2, line_w/2
     # set 1
     s[x][y] = 1
     y += 1
-    v = 2
+    v = 1
     for i in range(1, tour+1):
         n = 8*i
         for j in range(n):
+            v = next_v(s, x, y, v)
             s[x][y] = v
-            v += 1
+            if limit > 0 and v >= limit:
+                return s, x, y
             if j < n/4-1:
                 x -= 1 # go up
             elif j < n/2-1:
@@ -37,23 +34,36 @@ def create_spiral_tour(tour=0):
                 x += 1 # go down
             else:
                 y += 1 # go right
-    return s
+    return s, x, y-1 # one extra step on right
 
-def create_spiral(v):
-    return create_spiral_tour(step_from_value(v))
+# pprint(create_spiral_tour(0))
+# pprint(create_spiral_tour(3))
 
-def get_val_pos(s, v):
-    n = len(s[0])
-    for i in range(n):
-        for j in range(n):
-            if s[i][j] == v:
-                return i, j
+def step_from_value(v):
+    i = 0
+    r = 1
+    while r < v:
+        i += 1
+        r += 8*i
+    return i
 
 def get_1_dist(v):
-    s = create_spiral(v)
-    x, y = get_val_pos(s, v)
+    s, x, y = create_spiral_tour(step_from_value(v), v)
     pos1 = len(s[0])/2
     return abs(x - pos1) + abs(y - pos1)
 
 # out 1
 print(get_1_dist(368078))
+
+
+def next_v_2(s, x, y, v):
+    return s[x-1][y-1] + s[x][y-1] + s[x+1][y-1] + s[x-1][y] + s[x+1][y] + s[x-1][y+1] + s[x][y+1] + s[x+1][y+1]
+
+def get_2_val(v):
+    s, x, y = create_spiral_tour(300, v, next_v_2)
+    return s[x][y]
+
+# pprint(create_spiral_tour(5, 1000, next_v_2))
+
+# out 2
+print(get_2_val(368078))
