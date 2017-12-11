dac2db66413fa8c80ff85f997bace784c9cf00bf
Alexandre Adolphe <Alexandre_Adolphe@sigmadesigns.com>
2017-12-11 18:00:23 +0100

    Jour 20 de 2016

---
+++ 2016/Jour20.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+def make_blacklist(filename):
+    b = []
+    with open(filename) as f:
+        for l in f:
+            b.append(map(int, l.split("-")))
+    b.sort(key=lambda x: x[0])
+    # print b
+    p = 0
+    end = len(b)-1
+    while p < end:
+        if b[p][1]+1 >= b[p+1][0]:
+            if b[p+1][1] > b[p][1]:
+                b[p][1] = b[p+1][1]
+            b.pop(p+1)
+            end -= 1
+        else:
+            p += 1
+    return b
+
+blacklist = make_blacklist("input20")
+# print blacklist
+
+def reverse_list(l, maximum):
+    r = []
+    if l[0][0] > 0:
+        r.append([0, l[0][0]-1])
+    for i in range(len(l)-1):
+        r.append([l[i][1]+1, l[i+1][0]-1])
+    if l[-1][1] < maximum:
+        r.append([l[-1][1]+1, maximum])
+    return r
+
+ip = reverse_list(blacklist, 2**32-1)
+print ip
+
+print "low", ip[0][0]
+
+somme = 0
+for s,e in ip:
+    somme += e-s+1
+print somme, "{:,}".format(somme)
