1bf7c665215ad854ca69e67fc857942fddf27e78
Alexandre Adolphe <Alexandre_Adolphe@sigmadesigns.com>
2017-12-11 13:33:27 +0100

    Simplification J6 2016 (sans liste bouclé)

--- 2017/Jour6.el
+++ 2017/Jour6.el
@@ -5,24 +5,26 @@
   (let ((r (copy-tree elemts)))
     (setcdr (last r) r)))
 
-(defun distribute (len lst)
-  (let* ((val (car lst))
-	(n (/ val len))
-	(h (% val len))
-	(l (cdr lst)))
-    (setcar lst 0)
-    (dotimes (x h)
-      (setcar l (+ (car l) n 1))
-      (setq l (cdr l)))
-    (dotimes (x (- len h))
-      (setcar l (+ (car l) n))
-      (setq l (cdr l)))))
+(defun distribute (lst m)
+  (let* ((len (length lst))
+	 (val (nth m lst))
+	 (n (/ val len))
+	 (h (% val len))
+	 (l (nthcdr m lst))
+	 )
+    ;; (message (format "m %d, val %d, l %S" m val l))
+    (setcar l 0)
+    (dotimes (i len lst)
+      (setq l (if (cdr l) (cdr l) lst))
+      (if (< i h)
+	  (setcar l (+ (car l) n 1))
+	(setcar l (+ (car l) n))))))
 
 (defun sig (len lst)
   (let ((l (seq-subseq lst 0 len)))
     (concat (mapc 'number-to-string l))))
 
-(defun max (len lst)
+(defun u/max (len lst)
   (let ((m lst)
 	(l (cdr lst)))
     (dotimes (x len m)
@@ -30,21 +32,20 @@
 	(setq m l))
       (setq l (cdr l)))))
 
-(defun jour6 (input)
-  (let* ((len (length input))
-	 (lst (make-clist input))
-	 (ref (cons (sig len lst) nil))
-	 rpos)
-    ;; (print lst)
+(defun jour6 (lst)
+  (let ((ref (cons (copy-tree lst) nil))
+	rpos)
+    ;; (message (format "lst %S" lst))
     (while (not rpos)
-      (let ((m (max len lst)))
-	(distribute len m)
-	;; (print lst)
-	(let ((s (sig len lst)))
-	  ;; (message s)
-	  (setq rpos (seq-position ref s))
-	  (push s ref))))
+      (let ((m (seq-max lst)))
+	(distribute lst (seq-position lst m))
+	;; (message (format "lst %S" lst))
+	(setq rpos (seq-position ref lst))
+	(push (copy-tree lst) ref)
+	;; (message (format "ref %S" ref))
+	))
     `(,(+ 1 rpos) ,(- (length ref) 1))))
+
 
 (setq input '(0 2 7 0))
 (setq input6 '(0 5 10 0 11 14 13 4 11 8 8 7 1 4 12 11))
@@ -54,3 +55,30 @@
 
 (jour6 input6)
 (1695 7864)
+
+
+(defmacro measure-time (&rest body)
+  "Measure the time it takes to evaluate BODY."
+  `(let ((time (current-time)))
+     ,@body
+     (message "%.06f sec" (float-time (time-since time)))))
+
+(byte-compile 'distribute)
+(byte-compile 'jour6)
+(measure-time
+ (message (format "%S" (jour6 input6))))
+"4.729974 sec"
+
+(setq l (make-list 1000000 1))
+(measure-time
+ (seq-position l 2))
+
+(measure-time
+ (position 2 l))
+
+(setq v (make-vector 1000000 1))
+(measure-time
+ (seq-position v 2))
+
+(measure-time
+ (position 2 v))
