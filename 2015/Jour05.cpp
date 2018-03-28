cf22553b6918b45fbf24d388ce638ff75729e857
aad <aad@fukui.france.sdesigns.com>
2018-03-28 15:20:03 +0200

    Jour 5 2015
    regex :)

---
+++ 2015/Jour05.cpp
@@ -0,0 +1,80 @@
+
+
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cassert>
+#include <regex>
+
+bool is_nice(const std::string str)
+{
+	bool dletter = false;
+	int voyel = 0;
+	char last = ' ';
+
+	for (char c: str) {
+		switch (c) {
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+			voyel++;
+			break;
+		case 'b':
+		case 'd':
+		case 'q':
+		case 'y':
+			if (last == c-1)
+				// naughty
+				return false;
+		}
+		if (last == c)
+			dletter = true;
+		last = c;
+	}
+
+	return voyel >= 3 and dletter;
+}
+
+bool is_nice2(const std::string str)
+{
+	std::regex r1 ("(.)(.).*\\1\\2");
+	std::regex r2 ("(.).\\1");
+
+	return std::regex_search(str,r1) and std::regex_search(str, r2);
+}
+
+int main(int argc, char **argv)
+{
+	const char * inname = "input05";
+	std::ifstream infile = std::ifstream();
+	std::string line;
+	int count1 = 0, count2 = 0;
+
+	assert(is_nice("ugknbfddgicrmopn"));
+	assert(is_nice("aaa"));
+	assert(!is_nice("jchzalrnumimnmhp"));
+	assert(!is_nice("haegwjzuvuyypxyu"));
+	assert(!is_nice("dvszwmarrgswjxmb"));
+
+	assert(is_nice2("qjhvhtzxzqqjkmpb"));
+	assert(is_nice2("xxyxx"));
+	assert(!is_nice2("uurcxstgmygtbstg"));
+	assert(!is_nice2("ieodomkazucvgmuy"));
+	
+
+	if (argc > 1)
+		inname = argv[1];
+
+	infile.open(inname);
+
+	while (std::getline(infile, line)) {
+		count1 += is_nice(line);
+		count2 += is_nice2(line);
+	}
+
+	std::cout << "number of nice strings : 1 - " << count1 << ", 2 - " << count2 << std::endl;
+
+	return 0;
+}
