9ac3e66efd1fe10f2a0d68bb80882fe08cd65af4
Alexandre Adolphe <alexandre.adolphe@gmail.com>
2018-12-17 01:54:43 +0100

    Meilleur gestion des dépendances cargo dans le makefile
    Make est plus rapide pour gérer les dépendances que cargo du coup plus
    de FORCE sur les cibles cargo.

--- rules.mk
+++ rules.mk
@@ -1,40 +1,45 @@
 
 SRC_CPP := $(wildcard *.cpp)
 BIN_CPP := $(basename $(SRC_CPP))
-SRC_RS := $(wildcard */Cargo.toml)
-BIN_RS := $(SRC_RS:Cargo.toml=target)
-SRC_RSF := $(wildcard *.rs)
-BIN_RSF := $(basename $(SRC_RSF))
+SRC_RS := $(wildcard *.rs)
+BIN_RS := $(basename $(SRC_RS))
+SRC_CARGO := $(wildcard */Cargo.toml)
+CARGO_FOLDER := $(SRC_CARGO:/Cargo.toml=)
 
 CXXFLAGS := -std=c++11
 
-.PHONY: all
-all: $(BIN_CPP) $(BIN_RS) $(BIN_RSF)
+.DEFAULT_GOAL := all
 
 define create_target_cpp
 $(1): $(1).o
 	g++ -o $$@ $$< $$(LDFLAGS)
 endef
 
-define create_target_rs
-$(1): FORCE
-	cargo build --manifest-path $$@/../Cargo.toml
+define create_target_cargo
+BIN_CARGO += $(addprefix $(1),$(addprefix /target/debug/,$(1)))
+$$(lastword $$(BIN_CARGO)): $(wildcard $(1)/src/*.rs) $(1)/Cargo.toml
+	cargo build --manifest-path $$?
+	touch $$@
 endef
 
-define create_target_rsf
+define create_target_rs
 $(1): $(1).rs
 	rustc $$<
 endef
 
 $(foreach jour, $(BIN_CPP), $(eval $(call create_target_cpp, $(jour))))
+$(foreach jour, $(CARGO_FOLDER), $(eval $(call create_target_cargo, $(jour))))
 $(foreach jour, $(BIN_RS), $(eval $(call create_target_rs, $(jour))))
-$(foreach jour, $(BIN_RSF), $(eval $(call create_target_rsf, $(jour))))
+
+.PHONY: all
+all: $(BIN_CPP) $(BIN_CARGO) $(BIN_RS)
 
 %.o: %.cpp
 	g++ $(CXXFLAGS) -o $@ -c $<
 
 .PHONY: clean
 clean:
-	rm -rf $(BIN_CPP) $(SRC_CPP:.cpp=.o) $(BIN_RS) $(SRC_RS:.toml=.lock) $(BIN_RSF) $(SRC_RSF:.rs=.pdb)
+	rm -f $(BIN_CPP) $(SRC_CPP:.cpp=.o) $(BIN_RS) $(SRC_RS:.rs=.pdb)
+	rm -rf $(CARGO_FOLDER:%=%/target) $(SRC_CARGO:.toml=.lock)
 
 FORCE:
