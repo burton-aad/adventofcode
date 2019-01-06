d4d4e18b2b1bfaff840c6964e2f338c6263f5cba
Alexandre Adolphe <alexandre.adolphe@gmail.com>
2019-01-06 03:34:49 +0100

    Jour 19 2018

---
+++ 2018/Jour19.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import sys
+import argparse
+import re
+import copy
+from Jour16 import OPCODES
+
+
+class CPU:
+    def __init__(self, prog, ip_reg):
+        self.regs = [0]*6
+        self.ip = 0
+        self.ip_reg = ip_reg
+        self.prog = copy.deepcopy(prog)
+
+    def run(self):
+        while 0 <= self.ip < len(self.prog):
+            self.run_instruction()
+
+    def run_instruction(self):
+        self.regs[self.ip_reg] = self.ip
+        # print("ip={} {}".format(self.ip, self.regs), end=" ")
+        op, a, b, c = self.prog[self.ip]
+        # print("{} {} {} {}".format(op, a, b, c), end=" ")
+        OPCODES[op](self.regs, a, b, c)
+        # print(self.regs)
+        self.ip = self.regs[self.ip_reg]
+        self.ip += 1
+
+def parse_input(f):
+    r = re.compile(r"(.*) (\d+) (\d+) (\d+)")
+    prog = []
+    for l in f:
+        if l.startswith("#"):
+            ip = int(l.split()[-1])
+        else:
+            m = r.match(l)
+            prog.append([m.group(1), int(m.group(2)), int(m.group(3)), int(m.group(4))])
+    return ip, prog
+
+def jour19(f):
+    ip_reg, prog = parse_input(f)
+    print(ip_reg)
+    print(prog)
+
+    cpu = CPU(prog, ip_reg)
+    cpu.run()
+    print("Part 1:", cpu.regs[0])
+
+    # cpu = CPU(prog, ip_reg)
+    # cpu.regs[0] = 1
+    # cpu.run()
+    # print("Part 2:", cpu.regs[0])
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Jour 19 2018')
+    parser.add_argument('input_file', nargs='?', default="input19", help='the input file')
+    args = parser.parse_args(sys.argv[1:])
+    with open(args.input_file) as f:
+        jour19(f)
+
+
+#test
+# jour19(["#ip 0", "seti 5 0 1", "seti 6 0 2", "addi 0 1 0", "addr 1 2 3", "setr 1 0 0", "seti 8 0 4", "seti 9 0 5"])
