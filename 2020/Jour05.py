86e5e8dd4001a024bf353c75ada0375c21acb885
Alexandre Adolphe <alexandre.adolphe@gmail.com>
2020-12-07 01:44:13 +0100

    05/12/2020

---
+++ 2020/Jour05.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+
+def seat_id(s):
+    return int(s.translate(s.maketrans("FBLR", "0101")), 2)
+
+if __name__=="__main__":
+    with open(sys.argv[1]) as f:
+        l = sorted([seat_id(l.strip()) for l in f])
+    print("Part 1 : {}".format(max(l)))
+    r = [i for x,y in zip(l, l[1:]) for i in range(x+1, y) if y-x > 1]
+    print("Part 2 : {}".format(r[0]))
+
