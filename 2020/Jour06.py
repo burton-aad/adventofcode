1c62edc9af42882f56bcdf98901bdb75b41e5a97
Alexandre Adolphe <alexandre.adolphe@gmail.com>
2020-12-09 00:57:27 +0100

    06/12/2020

---
+++ 2020/Jour06.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys
+from functools import reduce
+
+def Jour06(f):
+    r1, r2 = 0, 0
+    while True:
+        s = [set(l) for l in iter(lambda: f.readline().strip(), "")]
+        if len(s) == 0:
+            break
+        r1 += len(reduce(lambda x, y: x | y, s))
+        r2 += len(reduce(lambda x, y: x & y, s))
+    print(f"Part 1 : {r1}, Part 2 : {r2}")
+
+if __name__=="__main__":
+    with open(sys.argv[1]) as f:
+        Jour06(f)
