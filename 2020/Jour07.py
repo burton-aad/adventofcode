e8a7d207f3618b7fcb39f576f2b0aeeb7d761fe0
Alexandre Adolphe <alexandre.adolphe@gmail.com>
2020-12-09 02:07:55 +0100

    07/12/2020

---
+++ 2020/Jour07.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+def Jour07(f):
+    d = {}
+    reverse_d = {}
+    regex = re.compile(r"(\d+)? ?(\w+ \w+) bags?")
+    for l in f:
+        m = regex.finditer(l)
+        cont_bag = next(m).group(2)
+        for b in m:
+            if b.group(2) != "no other":
+                d.setdefault(cont_bag, []).append((int(b.group(1)), b.group(2)))
+                reverse_d.setdefault(b.group(2), []).append((int(b.group(1)), cont_bag))
+
+    q = ["shiny gold"]
+    s = set()
+    while len(q) > 0:
+        k = q.pop()
+        for _, b in reverse_d.get(k, []):
+            s.add(b)
+            q.append(b)
+    print("Part 1:", len(s))
+
+    q = [(1, "shiny gold")]
+    r = 0
+    while len(q) > 0:
+        i, k = q.pop()
+        r += i
+        for j, b in d.get(k, []):
+            q.append((i*j, b))
+    print("Part 2:", r-1)
+
+if __name__=="__main__":
+    with open(sys.argv[1]) as f:
+        Jour07(f)
