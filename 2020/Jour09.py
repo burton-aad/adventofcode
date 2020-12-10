57d1060e3f0ef9face534dac5250abebb43807d7
Alexandre Adolphe <alexandre.adolphe@gmail.com>
2020-12-10 02:01:17 +0100

    09/12/2020

---
+++ 2020/Jour09.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import sys
+from collections import deque
+from itertools import combinations
+
+def valid(i, q):
+    return any(i == x + y for x,y in combinations(q, 2))
+
+def Jour09(transmit, preambule=25):
+    q = deque(transmit[:preambule])
+    for i in transmit[preambule:]:
+        if not valid(i, q):
+            print("Part 1:", i)
+            break
+        q.append(i)
+        q.popleft()
+
+    invalid = i
+    i, j = 0,2
+    while True:
+        s = sum(transmit[i:j])
+        if s == invalid:
+            print("Part 2:", min(transmit[i:j]) + max(transmit[i:j]))
+            break
+        elif s < invalid:
+            j += 1
+        else:
+            i += 1
+
+
+if __name__=="__main__":
+    with open(sys.argv[1]) as f:
+        transmit = [int(l) for l in f]
+    Jour09(transmit)
