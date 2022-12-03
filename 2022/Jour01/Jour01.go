eb7a8eb55b2f8043ae7323719a4a6ecaa185df9c
Alexandre Adolphe <alexandre.adolphe@gmail.com>
2022-12-03 14:55:10 +0100

    2022: Make utils funcs for go code.

--- 2022/Jour01/Jour01.go
+++ 2022/Jour01/Jour01.go
@@ -1,58 +1,25 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"sort"
 	"strconv"
+	"aoc/utils"
 )
-
-func readFile(path string) (s []string) {
-	readFile, err := os.Open(path)
-	if err != nil {
-		panic(err)
-	}
-	defer readFile.Close()
-
-	fileScanner := bufio.NewScanner(readFile)
-	fileScanner.Split(bufio.ScanLines)
-
-	for fileScanner.Scan() {
-		s = append(s, fileScanner.Text())
-	}
-
-	return
-}
-
-func mapSlice[T any, M any](a []T, f func(T) M) []M {
-	n := make([]M, 0, len(a))
-	for _, e := range a {
-		n = append(n, f(e))
-	}
-	return n
-}
-
-func sum(arr []int) (sum int) {
-	for _, value := range arr {
-		sum += value
-	}
-	return
-}
 
 type elf struct {
 	cals []int
 }
 
 func (e elf) sum_cals() int {
-	return sum(e.cals)
+	return utils.Sum(e.cals)
 }
 
 func main() {
 	elves := make([]elf, 1)
 	cur_elf := &elves[len(elves)-1]
 
-	s := readFile("input")
+	s := utils.ReadFileLines("input")
 	for _, val := range s {
 		if val == "" {
 			elves = append(elves, elf{})
@@ -68,5 +35,5 @@ func main() {
 
 	sort.Slice(elves, func(i, j int) bool { return elves[i].sum_cals() < elves[j].sum_cals() })
 	fmt.Println("Part 1:", elves[len(elves)-1].sum_cals())
-	fmt.Println("Part 2:", sum(mapSlice(elves[len(elves)-3:], func(e elf) int { return e.sum_cals() })))
+	fmt.Println("Part 2:", utils.Sum(utils.Map(elves[len(elves)-3:], func(e elf) int { return e.sum_cals() })))
 }
---
+++ 2022/utils/utils.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	"bufio"
+	"os"
+)
+
+func ReadFileLines(path string) (s []string) {
+	readFile, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer readFile.Close()
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		s = append(s, fileScanner.Text())
+	}
+
+	return
+}
+
+func Map[T any, M any](a []T, f func(T) M) []M {
+	n := make([]M, 0, len(a))
+	for _, e := range a {
+		n = append(n, f(e))
+	}
+	return n
+}
+
+func Sum(arr []int) (sum int) {
+	for _, value := range arr {
+		sum += value
+	}
+	return
+}
