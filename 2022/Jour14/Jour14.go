f1ef916a69b4e5d7d6094917f5bff7003a06a04d
Alexandre Adolphe <alexandre.adolphe@gmail.com>
2022-12-15 00:10:15 +0100

    14/12/2022 Part 2

--- 2022/Jour14/Jour14.go
+++ 2022/Jour14/Jour14.go
@@ -12,40 +12,25 @@ const (
 	AIR              = '.'
 	SAND             = 'o'
 	START_POINT      = '+'
+	START_X     int  = 500
 )
 
+type Pos struct {
+	x, y int
+}
+
 type Cave struct {
-	r                [][]byte
+	rocks            utils.Set[Pos]
 	xmin, xmax, ymax int
-	start            int
+	start            Pos
+	sand             utils.Set[Pos]
 }
 
 func parse_input(s []string) (c Cave) {
-	c.r = append(c.r, make([]byte, 1))
-	c.r[0][0] = START_POINT
-	c.xmin, c.xmax, c.ymax = 500, 500, 0
-
-	update_size := func(x, y int) {
-		// Pour simplifier on laisse toujours une colonne sur les coté et une ligne en plus en bas.
-		if x >= c.xmax {
-			nb := utils.MakeSlice[byte](x-c.xmax+1, AIR)
-			for i := range c.r {
-				c.r[i] = append(c.r[i], nb...)
-			}
-			c.xmax = x + 1
-		}
-		if x <= c.xmin {
-			for i := range c.r {
-				c.r[i] = append(utils.MakeSlice[byte](c.xmin-x+1, AIR), c.r[i]...)
-			}
-			c.xmin = x - 1
-		}
-		if y >= c.ymax {
-			for ; c.ymax <= y; c.ymax++ {
-				c.r = append(c.r, utils.MakeSlice[byte](len(c.r[0]), AIR))
-			}
-		}
-	}
+	c.rocks = *utils.NewSet[Pos]()
+	c.sand = *utils.NewSet[Pos]()
+	c.start = Pos{START_X, 0}
+	c.xmin, c.xmax, c.ymax = START_X, START_X, 0
 
 	for i := range s {
 		l := strings.Split(s[i], " -> ")
@@ -64,55 +49,90 @@ func parse_input(s []string) (c Cave) {
 					return x
 				}
 			})
-			update_size(lpos_s[0], lpos_s[1])
-			update_size(lpos_e[0], lpos_e[1])
+			c.update_size(lpos_s[0], lpos_s[1])
+			c.update_size(lpos_e[0], lpos_e[1])
 			xstart, xend := utils.MinMax(lpos_s[0], lpos_e[0])
 			ystart, yend := utils.MinMax(lpos_s[1], lpos_e[1])
 			for x := xstart; x <= xend; x++ {
 				for y := ystart; y <= yend; y++ {
-					c.r[y][x-c.xmin] = ROCK
+					c.rocks.Add(Pos{x, y})
 				}
 			}
 		}
 	}
-
-	c.start = utils.IndexOf(c.r[0], START_POINT)
 	return
 }
 
-func (c *Cave) print2d() {
-	for i := range c.r {
-		fmt.Println(string(c.r[i]))
+func (c *Cave) update_size(x, y int) {
+	// Pour simplifier on laisse toujours une colonne sur les coté et une ligne en plus en bas.
+	if x >= c.xmax {
+		c.xmax = x + 1
+	}
+	if x <= c.xmin {
+		c.xmin = x - 1
+	}
+	if y >= c.ymax {
+		c.ymax = y + 1
 	}
 }
 
-func (c *Cave) fall_sand() bool {
-	x, y, rest := c.start, 0, false
-	for ; y < len(c.r)-1 && !rest; y++ {
+func (c *Cave) print2d() {
+	for y := 0; y <= c.ymax; y++ {
+		line := utils.MakeSlice[byte](c.xmax-c.xmin+1, AIR)
+		for i, x := 0, c.xmin; x < c.xmax; i, x = i+1, x+1 {
+			p := Pos{x, y}
+			if p == c.start {
+				line[i] = START_POINT
+			} else if c.rocks.Contains(p) {
+				line[i] = ROCK
+			} else if c.sand.Contains(p) {
+				line[i] = SAND
+			}
+		}
+		fmt.Println(string(line))
+	}
+}
+
+func (c *Cave) fall_sand(endless_void bool) bool {
+	x, y, rest := c.start.x, c.start.y, false
+	for ; y < c.ymax && !rest; y++ {
 		rest = true
 		for _, n_pos := range []int{x, x - 1, x + 1} {
-			if c.r[y+1][n_pos] == AIR {
+			p := Pos{n_pos, y + 1}
+			if !c.rocks.Contains(p) && !c.sand.Contains(p) {
 				x = n_pos
 				rest = false
 				break
 			}
 		}
 	}
-	if rest {
-		c.r[y-1][x] = SAND
+
+	if !endless_void && c.start.x == x && c.start.y == y-1 {
+		rest = false // Sand is blocked
+	} else if rest {
+		c.sand.Add(Pos{x, y - 1})
+	} else if !endless_void {
+		c.sand.Add(Pos{x, y})
+		c.update_size(x, c.ymax-1)
+		rest = true
 	}
 	return rest
 }
 
 func main() {
-	s := utils.ReadFileLines("test")
+	s := utils.ReadFileLines("input")
 	detect := parse_input(s)
-	// detect.print2d()
 
 	i := 0
-	for detect.fall_sand() {
+	for detect.fall_sand(true) {
 		i++
 	}
-	detect.print2d()
+	// detect.print2d()
 	fmt.Println("Part 1: ", i)
+
+	for detect.fall_sand(false) {
+		i++
+	}
+	// detect.print2d()
+	fmt.Println("Part 2: ", i+1)
 }
