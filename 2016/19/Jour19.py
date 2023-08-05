cf6fbc9ec2b10ddd81315cc67ee879859331c25a
Alexandre Adolphe <alexandre.adolphe@gmail.com>
2023-08-05 18:19:42 +0200

    Correction test 19/2016 pour python3

--- 2016/19/Jour19.py
+++ 2016/19/Jour19.py
@@ -8,7 +8,7 @@ last  = [n, 1]
 t = 0
 
 while n > 2:
-    print "t", t, ", n", n, "->", first, last
+    print("t", t, ", n", n, "->", first, last)
     c = 2**t
     first[1] += c
     if n % 2 == 0:
@@ -17,7 +17,7 @@ while n > 2:
     else:
         first[0] += 2**(t+1)
         last[1] += first[1]
-    n /= 2
+    n //= 2
     t += 1
 
-print first[0], first[1] + last[1]
+print(first[0], first[1] + last[1])
